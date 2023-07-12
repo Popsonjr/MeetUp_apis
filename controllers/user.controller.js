05ad7a0b0daa3ba3aa44ca177be96e63b339f799
Muhammad Popoola <popoolama@yahoo.com>
2023-07-12 16:36:16 +0100

    authentication and routes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,3 +1,16 @@
-export const deleteUser = (req, res) => {
-    res.send("from controller")
+import User from "../models/user_model.js"
+import jwt  from "jsonwebtoken"
+import createError from "../utils/createError.js"
+
+export const deleteUser = async (req, res, next) => {
+    const user = await User.findById(req.params.id)
+
+    jwt.verify(token,process.env.JWT_KEY, async (err, payload) => {
+        if(req.userId !== user._id.toString()){
+            return next(createError(403, "you can delete only your account!")) 
+        }
+        await User.findByIdAndDelete(req.params.id)
+        res.status(200).send("Account deleted successfully")
+    })
+    
 }
